e89194b3a07f3dbc9f64358c1ac04e249ae1552e
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-08-31 16:05:02 +0100

    Use vue recommended rules

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   extends: ["plugin:vue/recommended", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": [{ multiline: { allowFirstLine: true } }],
+    "vue/mustache-interpolation-spacing": ["error", "never"]
   },
   parserOptions: {
     parser: "typescript-eslint-parser"
