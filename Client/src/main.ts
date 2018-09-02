6251e2a5126346d563fe2fad9e8558fae619de21
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-09-02 18:03:16 +0100

    Add vue-meta

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
+import Meta from "vue-meta";
 import App from "./App.vue";
 import router from "./Router";
 import store from "@/stores/Store";
 import "./RegisterServiceWorker";
 
 Vue.config.productionTip = false;
+Vue.use(Meta);
 
 new Vue({
   render: x => x(App),
