fd0fa7bde88f1274972eceedf79398a32379dd52
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-09-02 20:47:39 +0100

    Comment out Vuelidate

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,12 +1,15 @@
 import Vue from "vue";
 import Meta from "vue-meta";
+// import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import router from "./Router";
 import store from "@/stores/Store";
 import "./RegisterServiceWorker";
 
 Vue.config.productionTip = false;
+
 Vue.use(Meta);
+// Vue.use(Vuelidate);
 
 new Vue({
   render: x => x(App),
