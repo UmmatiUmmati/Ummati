7d76c582629d01dec6190f7003d77e40307ee196
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-09-03 11:11:21 +0100

    Add Dialogue component

---
+++ Client/src/components/Dialogue.vue
@@ -0,0 +1,43 @@
+<template>
+  <dialog>
+    <slot/>
+  </dialog>
+</template>
+
+<script lang="ts">
+// TODO: This does not work yet.
+// See https://github.com/GoogleChrome/dialog-polyfill/issues/169
+// See https://keithjgrant.com/posts/2018/meet-the-new-dialog-element/
+import Vue from "vue";
+import "dialog-polyfill/dialog-polyfill.js";
+
+const dialogPolyfill = (window as any).dialogPolyfill;
+delete (window as any).dialogPolyfill;
+
+export default Vue.extend({
+  props: {
+    open: {
+      default: false,
+      type: Boolean
+    }
+  },
+  watch: {
+    open(value: boolean) {
+      const dialog = this.$root;
+      if (value) {
+        (dialog as any).showModal();
+      } else {
+        (dialog as any).close();
+      }
+    }
+  },
+  created() {
+    const dialog = this.$root;
+    dialogPolyfill.registerDialog(dialog);
+  }
+});
+</script>
+
+<style lang="scss">
+@import "~dialog-polyfill/dialog-polyfill";
+</style>
