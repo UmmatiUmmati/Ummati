53d58e59705180a6dcefdf5216c4ba960e5c49ed
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-09-07 09:41:13 +0100

    Add OnlineStatus component

---
+++ Client/src/components/OnlineStatus.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <slot v-if="isOnline" name="online" />
+    <slot v-else name="offline" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const networkEvents = ["online", "offline"];
+function getOnlineStatus() {
+  if (navigator) {
+    return navigator.onLine || false
+  }
+  return true;
+}
+
+export default Vue.extend({
+  name: 'OnlineStatus',
+  data() {
+    return {
+      isOnline: getOnlineStatus()
+    };
+  },
+  created() {
+    networkEvents.forEach(event =>
+      window.addEventListener(event, this.updateStatus)
+    );
+  },
+  beforeDestroy() {
+    networkEvents.forEach(event =>
+      window.removeEventListener(event, this.updateStatus)
+    );
+  },
+  methods: {
+    updateStatus() {
+      this.isOnline = getOnlineStatus();
+      this.$emit('input', this.isOnline);
+    }
+  }
+});
+</script>
