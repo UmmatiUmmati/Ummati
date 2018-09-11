a120e3b018622ee08aaf1ef5dcaee9b1a1c6872a
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-09-11 09:27:45 +0100

    Use fade page transition

--- Client/src/Routes.ts
+++ Client/src/Routes.ts
@@ -5,36 +5,24 @@ const routes: RouteConfig[] = [
     path: "/",
     name: "home",
     component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue"),
-    meta: {
-      transition: "slide"
-    }
   },
   {
     path: "/about",
     name: "about",
     component: () =>
       import(/* webpackChunkName: "about" */ "./views/About.vue"),
-    meta: {
-      transition: "slide"
-    }
   },
   {
     path: "/search",
     name: "search",
     component: () =>
       import(/* webpackChunkName: "search" */ "./views/Search.vue"),
-    meta: {
-      transition: "slide"
-    }
   },
   {
     path: "/profile",
     name: "profile",
     component: () =>
       import(/* webpackChunkName: "profile" */ "./views/Profile.vue"),
-    meta: {
-      transition: "slide"
-    }
   }
 ];
 export default routes;
