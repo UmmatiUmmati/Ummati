ee1c390823a85fe5d65b2bf5cae6cb7246f5f960
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-10-24 11:58:21 +0100

    Add avatar component

---
+++ Source/Ummati/Client/src/components/Avatar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="avatar">
+    <img :alt="alt" :src="source" class="avatar-image"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Avatar",
+  props: {
+    alt: {
+      required: true,
+      type: String
+    },
+    source: {
+      required: true,
+      type: String
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+.avatar {
+  --avatar-border-radius: 50%;
+  --avatar-fallback-colour: #cccccc;
+  --avatar-size: 5em;
+
+  position: relative;
+  width: var(--avatar-size);
+  height: var(--avatar-size);
+  border-radius: var(--avatar-border-radius);
+  overflow: hidden;
+  background: var(--avatar-fallback-colour);
+}
+
+.avatar-image {
+  position: static;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
